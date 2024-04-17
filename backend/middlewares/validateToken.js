3cce92237865b71b4273aba3e741069d915d777a
Jose Miguel JGTECH <jmgg.95n@gmail.com>
2024-04-17 01:17:24 -0500

    fixing backend

--- backend/middlewares/validateToken.js
+++ backend/middlewares/validateToken.js
@@ -1,5 +1,4 @@
 import jwt from "jsonwebtoken";
-import fetch from "node-fetch";
 
 import { TOKEN_SECRET } from "../config.js";
 
@@ -15,43 +14,4 @@ export const authRequeried = (req, res, next) => {
     req.user = user;
     next();
   });
-};
-
-
-// Función para verificar el token de acceso
-export const verifyAccessToken = async (req, res, next) => {
-  const token = req.headers.authorization.split(" ")[1]; // Se espera que el token venga en el header Authorization
-  try {
-    // Paso 1: Obtener la clave pública de Google
-    const publicKeyResponse = await fetch(
-      "https://www.googleapis.com/oauth2/v3/certs"
-    );
-    const publicKeyData = await publicKeyResponse.json();
-    const publicKey = publicKeyData.keys[0].x5c[0];
-
-    // Paso 2: Verificar la firma del token
-    const verifiedToken = jwt.verify(token, publicKey, {
-      algorithms: ["RS256"],
-    });
-
-    // Paso 3: Verificar la validez del token
-    const currentTime = Math.floor(Date.now() / 1000);
-    if (verifiedToken.exp <= currentTime) {
-      return res.status(401).json({ error: "Token expirado" });
-    }
-
-    // Paso 4: Comprobar el alcance del token
-    if (!verifiedToken.scope.includes("email")) {
-      return res
-        .status(401)
-        .json({ error: "El token no tiene el alcance requerido" });
-    }
-
-    // Si llegamos hasta aquí, el token es válido
-    req.user = verifiedToken; // Agregamos el usuario decodificado al objeto de solicitud
-    next(); // Pasamos al siguiente middleware o controlador
-  } catch (error) {
-    console.error("Error al verificar el token:", error.message);
-    return res.status(401).json({ error: "Token inválido" });
-  }
 };
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -7,14 +7,14 @@ import {
   register,
   verifyToken
 } from "../controllers/auth.controller.js";
-import { authRequeried, verifyAccessToken } from "../middlewares/validateToken.js";
+import { authRequeried } from "../middlewares/validateToken.js";
 import { validateSchema } from "../middlewares/validator.middleware.js";
 import { registerSchema, loginSchema } from "../schemas/auth.schema.js";
 const router = Router();
 
 router.post("/register", validateSchema(registerSchema), register);
 router.post("/login", validateSchema(loginSchema),login);
-router.post("/google-login", verifyAccessToken, loginGoogle);
+router.post("/google-login",  loginGoogle);
 router.post("/logout", logout);
 router.get("/verify", verifyToken);
 router.get("/profile", authRequeried, profile);
